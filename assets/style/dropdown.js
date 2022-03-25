f67a3c61eb034896c46cd21321c3adc00cfabb35
wisnuprawira <wisnuprawira2000@gmail.com>
2022-03-26 02:34:17 +0700

    first commit

---
+++ assets/style/dropdown.js
@@ -0,0 +1,24 @@
+body {
+	margin:0;
+	padding:0;
+}
+
+.ScrollTop{
+	width: 50%;
+	height: 150px;
+	background: #84D4D7;
+	position: fixed;
+	top: 0;
+	left: 25%;
+	transition: .5s;
+}
+
+pre{
+	width: 50%;
+	height: 100vh;
+	background: red;
+	position: absolute;
+	top: 20%;
+	left: 25%;
+	z-index: -99;
+}
